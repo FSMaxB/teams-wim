f3a7e27b845e771a119e6123450e5484faeb6999
Max Bruckner <max@maxbruckner.de>
2018-09-16 13:28:40 +0200

    Use settings to actually ignore messages.

--- notifications.js
+++ notifications.js
@@ -71,33 +71,47 @@ class NewMessage {
 	}
 }
 
+// Determine if a message should trigger a notification (depending on the settings)
+function filterMessage(newMessage) {
+	if (newMessage.type !== 'Message') {
+		return false;
+	}
+
+	if (settings.ignoredConversations.includes(newMessage.conversation)) {
+		return false;
+	}
+
+	if (settings.ignoredUsers.includes(newMessage.sender)) {
+		return false;
+	}
+
+	if (settings.onlyImportant) {
+		return newMessage.isImportant;
+	}
+
+	return true;
+}
+
 function receive(json) {
 	if (json.eventMessages !== undefined) {
 		const eventMessages = json.eventMessages;
 		eventMessages
 			.map(json => new EventMessage(json))
-			.forEach(eventMessage => {
-				if (eventMessage.type === 'NewMessage') {
-					const newMessage = new NewMessage(eventMessage.resource);
-					if ((newMessage.type === 'Message') && (newMessage.messagetype.includes('Text'))) {
-						let title = 'New ';
-						if (newMessage.isImportant) {
-							title += 'IMPORTANT ';
-						}
-						if (newMessage.isTeamMessage) {
-							title += 'team ';
-						} else if (newMessage.isChatMessage) {
-							title += 'chat ';
-						}
-						title += `message from ${newMessage.sender}`;
+			.filter(eventMessage => eventMessage.type === 'NewMessage')
+			.map(eventMessage => new NewMessage(eventMessage.resource))
+			.filter(filterMessage)
+			.forEach(newMessage => {
+				let description = '';
+				description += newMessage.isImportant ? 'IMPORTANT ' : '';
+				description += newMessage.isTeamMessage ? 'team ' : '';
+				description += newMessage.isChatMessage ? 'chat ' : '';
 
-						browser.notifications.create({
-							type: 'basic',
-							'title': title,
-							'message': newMessage.plainContent,
-						});
-					}
-				}
+				const title = `New ${description} message from ${newMessage.sender}`;
+				browser.notifications.create({
+					type: 'basic',
+					'title': title,
+					'message': newMessage.plainContent,
+				});
 			});
 	}
 }
